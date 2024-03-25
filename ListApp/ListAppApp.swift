ff8c352b78286cbb9772dee46a23b2080b03954f
joomin <jmnl225@github.com>
2024-03-25 22:49:57 +0900

    Initial Commit

---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by joomin on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
