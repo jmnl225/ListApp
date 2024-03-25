a87f11401c0d004c4de22e293d2150d7040d2771
joomin <jmnl225@github.com>
2024-03-25 22:58:03 +0900

    test List

--- ListApp/ContentView.swift
+++ ListApp/ContentView.swift
@@ -7,13 +7,31 @@
 
 import SwiftUI
 
+struct Country: Identifiable {
+  var id = UUID()
+  var name: String
+}
+
+struct CountryRow: View {
+  var country: Country
+  
+  var body: some View {
+      Text("\(country.name)")
+  }
+}
+
 struct ContentView: View {
+    let first = Country(name: "Korea")
+    let second = Country(name: "Moon")
+    let third = Country(name: "Jigu")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let countries = [first, second, third]
+        
+        return VStack {
+             List(countries) { country in
+                  CountryRow(country:country)
+                }
         }
         .padding()
     }
